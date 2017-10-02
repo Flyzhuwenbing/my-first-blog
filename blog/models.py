db1dcdb702b9df6e04566e8d3a610694a4b6e66e
Flyzhuwenbing <wenbing.zhu@outlook.com>
2017-10-02 20:27:47 +0800

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+# 由于模型是保存在数据库中，修改模型后需执行python manage.py makegrations blog/python manage.py migrate blog
 from django.db import models
 from django.utils import timezone
 
@@ -6,12 +7,17 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now)
-    pulished_date = models.DateTimeField(blank=True,null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
-        self.publish_date = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
+
+
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
-#
+#连接模型与模板
 def post_list(request):
-    return render(request,'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # 通过查询集将模型传递到模板中
+    return render(request,'blog/post_list.html', {'posts':posts})
 
