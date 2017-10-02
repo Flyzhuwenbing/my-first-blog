b171a2ae67beb36fd4ee34978a6b2731d9d2652d
Flyzhuwenbing <wenbing.zhu@outlook.com>
2017-10-02 22:02:37 +0800

    add templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,4 +8,9 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # 通过查询集将模型传递到模板中
     return render(request,'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    # 查询集 将模型传递到post_detail.html中
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
 
